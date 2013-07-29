a4309d9ecfe52d9e8d9d72220e18b5363ce50a80
Matias <matias.partanen@jyu.fi>
2013-07-29 16:45:56 +0300

    Initial commit, making the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accountkeeper::Application.config.secret_token = '6aa10a93a963c71f365bee4cd9a6c9cfdd78dab65c569851313e84255921d779daa00163046f8e582a7b8544b9cfc78df3be22fafce6dcbd803ddcdbdffaa9cb'
