f8cc17311e04aa10a6666bb0cf90cf0c94956dac
Matias <matias.partanen@jyu.fi>
2013-08-07 22:50:34 +0300

    Finish user info edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -7,14 +10,39 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome!"    	
+      flash[:success] = "Welcome!"      
       redirect_to @user
     else
       render 'new'
     end
   end  
 
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit    
+  end
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end 
+
 end
